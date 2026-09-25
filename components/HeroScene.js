6e8dce0eb2b130c3a705ffa22893e177b352b68e
Antony Addy <antony@antonyaddy.com>
2026-09-25 17:46:42 +0200

    Remove hero scene renderer

--- components/HeroScene.js
+++ components/HeroScene.js
@@ -1,12 +1,3 @@
-'use client';
-
-import Image from 'next/image';
-
-export function HeroScene({ scene = 'home' }) {
-  return (
-    <div className={`hero-scene hero-scene--${scene}`} aria-hidden="true">
-      <Image src={`/images/heroes/${scene}.webp`} alt="" fill priority sizes="100vw"
-        className="hero-scene-photo" style={{ animation: 'none' }} />
-    </div>
-  );
+export function HeroScene() {
+  return null;
 }
