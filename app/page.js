c9131a12d773f63993610442877569966f50f81b
Antony Addy <antony@antonyaddy.com>
2026-09-25 17:46:08 +0200

    Remove home hero background artwork

--- app/page.js
+++ app/page.js
@@ -4,7 +4,6 @@ import Image from 'next/image';
 import { useApp } from '@/components/Providers';
 import { Icon } from '@/components/Icon';
 import { Logo } from '@/components/Logo';
-import { HeroScene } from '@/components/HeroScene';
 import { ROUTES } from '@/lib/routes';
 
 export default function HomePage() {
@@ -13,9 +12,6 @@ export default function HomePage() {
   return (
     <>
       <section className="hero-full">
-        <HeroScene scene="home" />
-        <div className="hero-scrim" />
-        <span className="hero-mark-bg"><Logo sizes="(max-width: 1080px) 170px, 300px" priority /></span>
         <div className="wrap hero-full-inner">
           <div className="hero-copy">
             <span className="kicker">{h.heroKicker}</span>
